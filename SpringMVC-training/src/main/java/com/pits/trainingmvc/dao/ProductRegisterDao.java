b3c3ff8cbf7eab5207e88f9b77a9ae49a3348b03
amaljanan.k <amaljanan.k@PIC4034.pitsolutions.com>
2021-05-27 13:51:21 +0530

    ProductRegister Dao class implementation

---
+++ SpringMVC-training/src/main/java/com/pits/trainingmvc/dao/ProductRegisterDao.java
@@ -0,0 +1,42 @@
+package com.pits.trainingmvc.dao;
+
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+import com.mysql.jdbc.Connection;
+import com.pits.trainingmvc.model.Product;
+
+public class ProductRegisterDao {
+
+	public boolean productRegister(Product product)
+	{
+		
+		String url = "jdbc:mysql://127.0.0.1:3306/test";
+		String u = "test";
+		String p = "password";
+		String sqlquery;
+
+		sqlquery = "insert into product(product_name,price) values(?,?)";
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con = (Connection) DriverManager.getConnection(url, u, p);
+			PreparedStatement pst = con.prepareStatement(sqlquery);
+
+			pst.setString(1, product.getProduct_name());
+			pst.setString(2, product.getPrice());
+
+			pst.executeUpdate();
+			pst.close();
+
+			return true;
+
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		
+		
+		return false;
+	}
+	
+}
--- SpringMVC-training/src/main/java/com/pits/trainingmvc/service/ProductRegisterService.java
+++ SpringMVC-training/src/main/java/com/pits/trainingmvc/service/ProductRegisterService.java
@@ -6,36 +6,21 @@ import java.sql.PreparedStatement;
 import org.springframework.stereotype.Service;
 
 import com.mysql.jdbc.Connection;
+import com.pits.trainingmvc.dao.ProductRegisterDao;
+import com.pits.trainingmvc.model.Product;
 
 @Service
 public class ProductRegisterService {
 
 	public boolean productRegister(String product_name, String price) {
-		String url = "jdbc:mysql://127.0.0.1:3306/test";
-		String u = "test";
-		String p = "password";
-		String sqlquery;
-
-		sqlquery = "insert into product(product_name,price) values(?,?)";
-
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			Connection con = (Connection) DriverManager.getConnection(url, u, p);
-			PreparedStatement pst = con.prepareStatement(sqlquery);
-
-			pst.setString(1, product_name);
-			pst.setString(2, price);
-
-			pst.executeUpdate();
-			pst.close();
-
-			return true;
-
-		} catch (Exception e) {
-			System.out.println(e);
-		}
-
-		return false;
+		
+		ProductRegisterDao productRegisterDao = new ProductRegisterDao();
+		
+		Product product =  new Product();
+		product.setProduct_name(product_name);	
+		product.setPrice(price);
+			
+		return productRegisterDao.productRegister(product);
 	}
 
 }
