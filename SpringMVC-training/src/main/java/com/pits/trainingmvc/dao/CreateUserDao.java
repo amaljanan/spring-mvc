c890595f832376311e625417b7b99d31978d0955
amaljanan.k <amaljanan.k@PIC4034.pitsolutions.com>
2021-05-27 12:49:37 +0530

    CreateUserDao layer implementation

---
+++ SpringMVC-training/src/main/java/com/pits/trainingmvc/dao/CreateUserDao.java
@@ -0,0 +1,51 @@
+package com.pits.trainingmvc.dao;
+
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+import com.mysql.jdbc.Connection;
+import com.pits.trainingmvc.model.User;
+
+public class CreateUserDao {
+
+	public boolean CreateUser(User user)
+	{
+		
+		String url = "jdbc:mysql://127.0.0.1:3306/test";
+		String u = "test";
+		String p = "password";
+		String sqlquery;
+		//String admin_name = "";
+
+		/*
+		 * if (user.getRole() == 1) { sqlquery =
+		 * "insert into user(uname,normal_pass,role) values(?,?,?)"; admin_name =
+		 * user.getUser_name() + "@pits.com"; } else { sqlquery =
+		 * "insert into user(uname,normal_pass,role) values(?,?,?)"; }
+		 */
+		sqlquery = "insert into user(uname,normal_pass,role) values(?,?,?)";
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con = (Connection) DriverManager.getConnection(url, u, p);
+			PreparedStatement pst = con.prepareStatement(sqlquery);
+			
+				pst.setString(1, user.getUser_name());
+				pst.setString(2, user.getPassword());
+				pst.setInt(3, user.getRole());
+				
+				if(user.getRole()==1)
+					pst.setString(1, user.getUser_name()+"@pits.com");
+			
+				pst.executeUpdate();
+				pst.close();
+
+			return true;
+
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		return false;
+	}
+	
+}
--- SpringMVC-training/src/main/java/com/pits/trainingmvc/service/CreateUserService.java
+++ SpringMVC-training/src/main/java/com/pits/trainingmvc/service/CreateUserService.java
@@ -7,46 +7,22 @@ import java.sql.ResultSet;
 import org.springframework.stereotype.Service;
 
 import com.mysql.jdbc.Connection;
+import com.pits.trainingmvc.dao.CreateUserDao;
+import com.pits.trainingmvc.model.User;
 
 @Service
 public class CreateUserService {
 
 	public boolean userCreate(String username, String password, int role) {
-		String url = "jdbc:mysql://127.0.0.1:3306/test";
-		String u = "test";
-		String p = "password";
-		String sqlquery;
-		String admin_name = "";
-
-		if (role == 1) {
-			sqlquery = "insert into user(uname,normal_pass,role) values(?,?,?)";
-			admin_name = username + "@pits.com";
-		} else {
-			sqlquery = "insert into user(uname,normal_pass,role) values(?,?,?)";
-		}
-
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			Connection con = (Connection) DriverManager.getConnection(url, u, p);
-			PreparedStatement pst = con.prepareStatement(sqlquery);
+		
+		User user = new User();
+			user.setUser_name(username);
+			user.setPassword(password);
+			user.setRole(role);
 			
-				pst.setString(1, username);
-				pst.setString(2, password);
-				pst.setInt(3, role);
-				
-				if(role==1)
-					pst.setString(1, admin_name);
-			
-				pst.executeUpdate();
-				pst.close();
-
-			return true;
-
-		} catch (Exception e) {
-			System.out.println(e);
-		}
-
-		return false;
+		CreateUserDao createUserDao = new CreateUserDao();
+		
+		return createUserDao.CreateUser(user);
 	}
 
 }
